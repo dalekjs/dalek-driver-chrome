7f2be06a1d62145baa92f5eaeaa36486c79eb453
Rodney Rehm <mail+github@rodneyrehm.de>
2015-04-26 21:17:40 +0200

    feature(capability): adding hostNotFoundRedirect

--- capability.js
+++ capability.js
@@ -1,6 +1,8 @@
 module.exports = {
   // desribes the URL the browser redirects to when it cannot resolve the requested host
   'POST :sessionId/url hostNotFoundUrl': 'data:text/html,chromewebdata',
+  // desribes if the browser redirects to a standard URL when it cannot resolve the requested host
+  'POST :sessionId/url hostNotFoundRedirect': true,
   // describes when the command is finished
   'POST :sessionId/url resolve': 'DOMContentLoaded',
 };
